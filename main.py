b9087284605610b119ea94a14893314fa3693457
kaseydoan <kdoan024@ucr.edu>
2024-12-12 22:52:50 -0800

    Added Back feature selection class

--- main.py
+++ main.py
@@ -53,6 +53,78 @@ class Validator:
 
         return correct_predictions / num_instances
 
+class FeatureSelection:
+    def __init__(self, num_features, data, labels):
+        self.num_features = num_features
+        self.data = data
+        self.labels = labels
+        self.validator = Validator()
+
+    def forward_selection(self):
+        current_features = set()
+        best_features = set()
+        best_accuracy = 0
+
+        print(f"Using no features and leave-one-out validation, I get an accuracy of {best_accuracy:.4f}")
+        print("Beginning search.")
+
+        while len(current_features) < self.num_features:
+            best_local_accuracy = 0
+            best_new_feature = None
+
+            for feature in range(1, self.num_features + 1):
+                if feature not in current_features:
+                    test_features = current_features | {feature}
+                    accuracy = self.validator.validate(self.data, self.labels, feature_subset=[f - 1 for f in test_features])
+
+                    print(f"Using feature(s) {sorted(list(test_features))} accuracy is {accuracy:.4f}")
+
+                    if accuracy > best_local_accuracy:
+                        best_local_accuracy = accuracy
+                        best_new_feature = feature
+
+            if best_new_feature is not None:
+                current_features.add(best_new_feature)
+                print(f"Feature set {sorted(list(current_features))} was best, accuracy is {best_local_accuracy:.4f}")
+
+                if best_local_accuracy > best_accuracy:
+                    best_accuracy = best_local_accuracy
+                    best_features = current_features.copy()
+
+        print(f"Finished search!! The best feature subset is {sorted(list(best_features))}, which has an accuracy of {best_accuracy:.4f}")
+
+    def backward_elimination(self):
+        current_features = set(range(1, self.num_features + 1))
+        best_features = current_features.copy()
+        best_accuracy = self.validator.validate(self.data, self.labels, feature_subset=[f - 1 for f in current_features])
+
+        print(f"Using all features and leave-one-out validation, I get an accuracy of {best_accuracy:.4f}")
+        print("Beginning search.")
+
+        while len(current_features) > 1:
+            best_local_accuracy = 0
+            best_feature_to_remove = None
+
+            for feature in current_features:
+                test_features = current_features - {feature}
+                accuracy = self.validator.validate(self.data, self.labels, feature_subset=[f - 1 for f in test_features])
+
+                print(f"Using feature(s) {sorted(list(test_features))} accuracy is {accuracy:.4f}")
+
+                if accuracy > best_local_accuracy:
+                    best_local_accuracy = accuracy
+                    best_feature_to_remove = feature
+
+            if best_feature_to_remove is not None:
+                current_features.remove(best_feature_to_remove)
+                print(f"Feature set {sorted(list(current_features))} was best, accuracy is {best_local_accuracy:.4f}")
+
+                if best_local_accuracy > best_accuracy:
+                    best_accuracy = best_local_accuracy
+                    best_features = current_features.copy()
+
+        print(f"Finished search!! The best feature subset is {sorted(list(best_features))}, which has an accuracy of {best_accuracy:.4f}")
+
 def load_data(file):
     data = np.loadtxt(file)
 
